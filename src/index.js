3e456bbe54fd96108a5ac33d267323f63b94a140
Bob <petrbobe@seznam.cz>
2024-06-20 16:01:04 +0200

    js function to add and click items

--- src/index.js
+++ src/index.js
@@ -6,3 +6,30 @@ window.onload = () => {
                .register('./sw.js');
     }
   }
+
+const input = document.getElementById("input-txt");
+const list = document.getElementById("list-id");
+
+function addItem() {
+  if (input.value === '') {
+    alert("Text musí být vyplněn");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = input.value;
+    li.classList.add("list_item")
+    list.appendChild(li);
+    let div = document.createElement("div");
+    div.innerHTML = "\u00d7";
+    li.appendChild(div);
+  }
+
+  input.value = "";
+}
+
+list.addEventListener("click", function(e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("list_item--checked");
+  } else if (e.target.tagName === "DIV") {
+    e.target.parentElement.remove();
+  }
+}, false);
