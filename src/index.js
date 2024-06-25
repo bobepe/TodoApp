fa34739443627df0034d5e279504658055afeb18
Bob <petrbobe@seznam.cz>
2024-06-25 14:15:05 +0200

    verze

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
-window.onload = () => {
-    'use strict';
-  
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-               .register('./sw.js');
-    }
+/*window.onload = () => {
+  'use strict';
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+              .register('./sw.js');
   }
+}*/
+
+window.onload = () => {
+  'use strict';
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('./sw.js')
+      .then((registration) => {
+        registration.onupdatefound = () => {
+          const installingWorker = registration.installing;
+          installingWorker.onstatechange = () => {
+            if (installingWorker.state === 'installed') {
+              if (navigator.serviceWorker.controller) {
+                // Nová aktualizace je k dispozici
+                console.log('Nový obsah je dostupný, prosím obnovte stránku.');
+                if (confirm("Nová verze aplikace je k dispozici. Chcete stránku nyní obnovit?")) {
+                  window.location.reload();
+                }
+              } else {
+                // Obsah je uložen pro offline použití
+                console.log('Obsah je uložen pro offline použití.');
+              }
+            }
+          };
+        };
+      })
+      .catch((error) => {
+        console.error('Chyba při registraci service workeru:', error);
+      });
+  }
+};
 
 const input = document.getElementById("input-txt");
 const list = document.getElementById("list-id");
